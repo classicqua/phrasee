59229cec8db4ddb63a69fa343a86f00f992fe5f8
hirosasaki <classic.qua@gmail.com>
2013-02-06 15:57:56 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phrasee::Application.config.secret_token = 'b6ed36cddf10c01f9b4f521e0d48edf3072f912bdab0c59e7045b98b07875a63f8b0a538e2d43ad2ac31eb9bc887f0f0f204fc4f68b7025207adb17ce9b847aa'
