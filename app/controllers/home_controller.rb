961bda5bfe1e240b975877679b04ec54b084f7a0
hirosasaki <classic.qua@gmail.com>
2013-02-07 18:33:32 +0900

    一時的にHomeControllerの定数使ってる部分削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @categories = Category.all
-    @phrases = Phrase.order('updated_at DESC').limit(Constants.PH_LIMIT_INDEX_PHRASE_NUM)
+    #@phrases = Phrase.order('updated_at DESC').limit(Constants.PH_LIMIT_INDEX_PHRASE_NUM)
+    @phrases = Phrase.order('updated_at DESC')
   end
 end
