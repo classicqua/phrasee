ce61bc6272362a45a277040f554cc44abdea440e
hirosasaki <classic.qua@gmail.com>
2013-02-07 15:48:16 +0900

    annotate実行した

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: phrases
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  japanese    :string(255)
+#  english     :string(255)
+#  user_id     :integer
+#  category_id :integer
+#  source      :string(255)
+#  story       :text
+#  launch_flg  :boolean
+#
+
 class Phrase < ActiveRecord::Base
   attr_accessible :japanese, :english, :source, :story, :launch_flg, :category_id
   belongs_to :user
--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: phrases
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  japanese    :string(255)
+#  english     :string(255)
+#  user_id     :integer
+#  category_id :integer
+#  source      :string(255)
+#  story       :text
+#  launch_flg  :boolean
+#
+
 require 'spec_helper'
 
 describe Phrase do
