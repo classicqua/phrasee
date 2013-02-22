edf6df210b98d71f9761e2b741fec96862a66214
hirosasaki <classic.qua@gmail.com>
2013-02-22 15:20:06 +0900

    confirmedの人のみ一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   #before_filter :authenticate_user!
 
   def index
-    @users = User.paginate(page:params[:page], per_page:10)
+    # 本人確認済の人だけ一覧表示
+    @users = User.where("confirmed_at is NOT NULL").paginate(page:params[:page], per_page:10)
   end
 
   def show
